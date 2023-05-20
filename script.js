4f584ff92dc0dfdf2a657c4ede5ecb0136db2572
L. Thackeray <lthackeray33@gmail.com>
2023-05-20 18:56:17 -0400

    Created getComputerChoice function and computerChoice variable

---
+++ script.js
@@ -0,0 +1,16 @@
+let computerChoice = '';
+
+let getComputerChoice = () => {
+    let randNum = Math.floor(Math.random() * 9) + 1;
+    switch (randNum) {
+        case 1, 2, 3:
+            computerChoice = 'rock';
+            break;
+        case 4, 5, 6:
+            computerChoice = 'paper';
+            break;
+        case 7, 8, 9:
+            computerChoice = 'scissors';
+            break;
+    }
+}
