fd8cb2a5caaf0127f0b175229ce235747c6f8c87
L. Thackeray <lthackeray33@gmail.com>
2023-05-20 19:46:01 -0400

    Saved player response to variable and set it to lowercase to match computerChoice

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ function getComputerChoice() {
 }
 
 const computerChoice = getComputerChoice();
+const playerChoice = prompt('Enter rock, paper, or scissors').toLowerCase();
+
+function game(computerChoice, playerChoice) {
+    let validAnswer = true;
+    if (playerChoice !== 'rock' || playerChoice !== 'paper' || playerChoice !== 'scissors') {
+        validAnswer = false;
+        playerChoice = prompt(`You didn't enter a valid move! Please enter rock, paper, or scissors.`);
+        if(playerChoice === computerChoice) {
+            alert('You won!');
+        } else {
+            alert('You lost!')
+        }
+    } else {
+        while(!validAnswer) {
+            
+        }
+        
+    }
+}
+
+game(computerChoice, playerChoice);
+
+/* while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+            playerChoice = prompt(`You didn't enter a valid move! Please enter rock, paper, or scissors.`);
+        } */
